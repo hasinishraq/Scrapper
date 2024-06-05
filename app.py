0982d1ec606a012d944d562081f2f15e2d5e45e5
hasinishraq <hasinishraq26@gmail.com>
2024-06-05 10:31:53 +0600

    is it

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ import os
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Global variable to store notices
+notices = []
+
 def get_notice_links(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
-        notices = soup.find_all('div', class_='notice')
+        notice_elements = soup.find_all('div', class_='notice')
         notice_list = []
 
-        for notice in notices:
+        for notice in notice_elements:
             a_tag = notice.find('a', href=True)
             if a_tag:
                 notice_title = a_tag.get_text(strip=True)
@@ -29,21 +32,39 @@ def get_notice_links(url):
         return []
 
 def monitor_notices():
+    global notices
     notice_url = 'https://www.uiu.ac.bd/notice'
     while True:
         logging.info("Checking for new notices...")
         notices = get_notice_links(notice_url)
-        
+        # Print notices to console
         for title, link in notices:
             print(f"Notice: {title}\nLink: {link}\n")
         
         # Wait for an hour before checking again
         time.sleep(3600)
 
+class NoticeHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            
+            html_content = '<html><head><title>Notices</title></head><body>'
+            html_content += '<h1>Latest Notices</h1><ul>'
+            for title, link in notices:
+                html_content += f'<li><a href="{link}" target="_blank">{title}</a></li>'
+            html_content += '</ul></body></html>'
+            
+            self.wfile.write(html_content.encode('utf-8'))
+        else:
+            super().do_GET()
+
 def start_http_server():
     port = int(os.environ.get("PORT", 10000))
     server_address = ('0.0.0.0', port)
-    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    httpd = HTTPServer(server_address, NoticeHandler)
     logging.info(f"Starting HTTP server on port {port}")
     httpd.serve_forever()
 
